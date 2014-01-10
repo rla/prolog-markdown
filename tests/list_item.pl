440be4ac3274da82d28bd60171c09493b13ad596
Raivo Laanemets <raivo@infdot.com>
2014-01-10 05:11:38 +0200

    List item tests for oredered lists

--- tests/list_item.pl
+++ tests/list_item.pl
@@ -10,6 +10,13 @@ md_bullet_list_item_string(String, Item, Mode, Rest):-
     string_codes(Item, ItemCodes),
     Mode = ModeTmp.
 
+md_ordered_list_item_string(String, Item, Mode, Rest):-
+    string_codes(String, Codes),
+    phrase(md_ordered_list_item(ItemCodes, ModeTmp), Codes, RestCodes), !,
+    string_codes(Rest, RestCodes),
+    string_codes(Item, ItemCodes),
+    Mode = ModeTmp.
+
 % Bullet list item. Star bullet.
 
 test(list_item_1):-
@@ -64,5 +71,47 @@ test(list_item_10):-
 
 test(list_item_11):-
     md_bullet_list_item_string("* abc\ndef\n\nghi", "abc\ndef", normal, "ghi").
+
+% Bullet list item, contains inline code.
+
+test(list_item_12):-
+    md_bullet_list_item_string("* abc\n        code", "abc\n    code", normal, "").
+
+% Ordered list item.
+
+test(list_item_13):-
+    md_ordered_list_item_string("1. abc", "abc", normal, "").
+
+% Ordered list item. Multiple lines.
+
+test(list_item_14):-
+    md_ordered_list_item_string("1. abc\ndef", "abc\ndef", normal, "").
+
+% Ordered list item. Indented.
+
+test(list_item_15):-
+    md_ordered_list_item_string(" 1. abc\ndef", "abc\ndef", normal, "").
+
+% Ordered list item. Ends with empty line.
+
+test(list_item_16):-
+    md_ordered_list_item_string(" 1. abc\ndef\n\nghi", "abc\ndef", normal, "ghi").
+
+% Ordered list item. Paragraph mode.
+
+test(list_item_17):-
+    md_ordered_list_item_string(" 1. abc\ndef\n\n2. ghi", "abc\ndef", para, "2. ghi").
+
+% No ordered list item.
+
+test(list_item_18):-
+    (   md_ordered_list_item_string(" 1\\. abc", _, _, _)
+    ->  fail
+    ;   true).
+
+% Ordered list item. Ends with new list item.
+
+test(list_item_19):-
+    md_ordered_list_item_string(" 1. abc\ndef\n2. ghi", "abc\ndef", normal, "2. ghi").
 
 :- end_tests(md_list_item).
